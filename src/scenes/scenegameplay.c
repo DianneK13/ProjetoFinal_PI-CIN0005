7f92b131b7ff96b7f29c3c6069aa86d6432612c3
lysanor <marcosalexandre.li@hotmail.com>
2025-08-10 16:39:43 -0300

    mexi nos objetos e adicionei assets

--- src/scenes/scenegameplay.c
+++ src/scenes/scenegameplay.c
@@ -70,113 +70,113 @@ GameContext InitializeGameplayState(GameContext context) {
     // Initialize objects with their properties
     context.gameplay.objects[ID_GAMEPLAY_ARMARIO].name = "armario";
     context.gameplay.objects[ID_GAMEPLAY_ARMARIO].id = ID_GAMEPLAY_ARMARIO;
-    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].texture = LoadTexture("assets/objetosMainGameplay/Armario.png");
+    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_ARMARIO].bounds = (Rectangle){0, 180, 242, 455};
     context.gameplay.objects[ID_GAMEPLAY_ARMARIO].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_ARMARIO].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].name = "caixaArmario";
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].id = ID_GAMEPLAY_CAIXA_ARMARIO;    
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].texture = LoadTexture("assets/objetosMainGameplay/CaixaArmario.png");
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].bounds = (Rectangle){66, 100, 140, 73};
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_ARMARIO].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].name = "caixaCama";
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].id = ID_GAMEPLAY_CAIXA_CAMA;
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].texture = LoadTexture("assets/objetosMainGameplay/CaixaCama.png");
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].bounds = (Rectangle){957, 710, 118, 75};
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_CAIXA_CAMA].type = INTERACTIVE;    
 
     context.gameplay.objects[ID_GAMEPLAY_BURACO].name = "buraco";
     context.gameplay.objects[ID_GAMEPLAY_BURACO].id = ID_GAMEPLAY_BURACO;
-    context.gameplay.objects[ID_GAMEPLAY_BURACO].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_BURACO].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_BURACO].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_BURACO].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_BURACO].texture = LoadTexture("assets/.png"); //tá sem textura ainda
+    context.gameplay.objects[ID_GAMEPLAY_BURACO].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_BURACO].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_BURACO].bounds = (Rectangle){321, 739, 22, 22};
     context.gameplay.objects[ID_GAMEPLAY_BURACO].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_BURACO].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_DIARIO].name = "diario";
     context.gameplay.objects[ID_GAMEPLAY_DIARIO].id = ID_GAMEPLAY_DIARIO;
-    context.gameplay.objects[ID_GAMEPLAY_DIARIO].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_DIARIO].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_DIARIO].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_DIARIO].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_DIARIO].texture = LoadTexture("assets/objetosMainGameplay/Diario.png");
+    context.gameplay.objects[ID_GAMEPLAY_DIARIO].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_DIARIO].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_DIARIO].bounds = (Rectangle){563, 417, 58, 54};
     context.gameplay.objects[ID_GAMEPLAY_DIARIO].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_DIARIO].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_VITROLA].name = "vitrola";
     context.gameplay.objects[ID_GAMEPLAY_VITROLA].id = ID_GAMEPLAY_VITROLA;
-    context.gameplay.objects[ID_GAMEPLAY_VITROLA].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_VITROLA].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_VITROLA].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_VITROLA].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_VITROLA].texture = LoadTexture("assets/objetosMainGameplay/Vitrola_.png");
+    context.gameplay.objects[ID_GAMEPLAY_VITROLA].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_VITROLA].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_VITROLA].bounds = (Rectangle){390, 313, 105, 153};
     context.gameplay.objects[ID_GAMEPLAY_VITROLA].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_VITROLA].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_GARRAFA].name = "garrafa";
     context.gameplay.objects[ID_GAMEPLAY_GARRAFA].id = ID_GAMEPLAY_GARRAFA;
-    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].texture = LoadTexture("assets/objetosMainGameplay/Garrafa.png");
+    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_GARRAFA].bounds = (Rectangle){725, 393, 45, 79};
     context.gameplay.objects[ID_GAMEPLAY_GARRAFA].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_GARRAFA].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_ESTANTE].name = "estante";
     context.gameplay.objects[ID_GAMEPLAY_ESTANTE].id = ID_GAMEPLAY_ESTANTE;
-    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].bounds = (Rectangle){50, 100, 150, 120};
+    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].texture = LoadTexture("assets/objetosMainGameplay/Estante.png");
+    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_ESTANTE].bounds = (Rectangle){905, 191, 157, 101};
     context.gameplay.objects[ID_GAMEPLAY_ESTANTE].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_ESTANTE].type = INTERACTIVE;    
 
     context.gameplay.objects[ID_GAMEPLAY_PIRATA].name = "pirata";
     context.gameplay.objects[ID_GAMEPLAY_PIRATA].id = ID_GAMEPLAY_PIRATA;
-    context.gameplay.objects[ID_GAMEPLAY_PIRATA].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_PIRATA].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_PIRATA].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_PIRATA].bounds = (Rectangle){50, 100, 150, 120};       
+    context.gameplay.objects[ID_GAMEPLAY_PIRATA].texture = LoadTexture("assets/objetosMainGameplay/Pirata.png");
+    context.gameplay.objects[ID_GAMEPLAY_PIRATA].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_PIRATA].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_PIRATA].bounds = (Rectangle){873, 377, 156, 183};     
     context.gameplay.objects[ID_GAMEPLAY_PIRATA].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_PIRATA].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].name = "varaPesca";
     context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].id = ID_GAMEPLAY_VARA_PESCA;
-    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].bounds = (Rectangle){50, 100, 150, 120};     
+    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].texture = LoadTexture("assets/objetosMainGameplay/VaraPesca.png");
+    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].size = (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT};
+    context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].bounds = (Rectangle){0, 0, 0, 0};   //preciso pegar a imagem com todas as camadas com malu
     context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_VARA_PESCA].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_CHAVE].name = "chave";
     context.gameplay.objects[ID_GAMEPLAY_CHAVE].id = ID_GAMEPLAY_CHAVE;
-    context.gameplay.objects[ID_GAMEPLAY_CHAVE].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_CHAVE].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_CHAVE].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_CHAVE].bounds = (Rectangle){50, 100, 150, 120};   
+    context.gameplay.objects[ID_GAMEPLAY_CHAVE].texture = LoadTexture("assets/jill.png");
+    context.gameplay.objects[ID_GAMEPLAY_CHAVE].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_CHAVE].size = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_CHAVE].bounds = (Rectangle){0, 0, 0, 0};  
     context.gameplay.objects[ID_GAMEPLAY_CHAVE].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_CHAVE].type = INTERACTIVE;
 
     context.gameplay.objects[ID_GAMEPLAY_BILHETE].name = "bilhete";
     context.gameplay.objects[ID_GAMEPLAY_BILHETE].id = ID_GAMEPLAY_BILHETE;
-    context.gameplay.objects[ID_GAMEPLAY_BILHETE].texture = LoadTexture("assets/.png");
-    context.gameplay.objects[ID_GAMEPLAY_BILHETE].position = (Vector2){50, 100};
-    context.gameplay.objects[ID_GAMEPLAY_BILHETE].size = (Vector2){150, 120};
-    context.gameplay.objects[ID_GAMEPLAY_BILHETE].bounds = (Rectangle){50, 100, 150, 120};         
+    context.gameplay.objects[ID_GAMEPLAY_BILHETE].texture = LoadTexture("assets/jill.png");
+    context.gameplay.objects[ID_GAMEPLAY_BILHETE].position = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_BILHETE].size = (Vector2){0, 0};
+    context.gameplay.objects[ID_GAMEPLAY_BILHETE].bounds = (Rectangle){0, 0, 0, 0};         
     context.gameplay.objects[ID_GAMEPLAY_BILHETE].state = ORIGINAL;
     context.gameplay.objects[ID_GAMEPLAY_BILHETE].type = INTERACTIVE;
 
-    context.gameplay.background = LoadTexture("assets/fundofodao.png");
+    context.gameplay.background = LoadTexture("assets/MainGameplayBG.png");
     context.gameplay.substate = GAMEPLAY_SUBSTATE_MAIN;
     return context;
 }
